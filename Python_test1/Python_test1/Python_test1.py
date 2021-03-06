ed6f2035091d3d1b0a7a6928d533cd54043edcbf
byl18 <65812792+byl18@users.noreply.github.com>
2021-03-06 19:39:00 +0800

    Add files via upload

---
+++ Python_test1/Python_test1/Python_test1.py
@@ -0,0 +1,8 @@
+﻿list2 = [1,2,1,3,8]
+list1=list2.copy()
+tuples=tuple(list2)
+print(type(list1),type(list2),type(tuples),any(list1))
+
+#list2[1:2]=[]
+#for content in mylist+list2:
+#    print(content,end="\n")
